ce2553418428909922c23c6708923ab6d01f0823
Ruqia alsheikha <rokiash654@gmail.com>
2026-05-24 19:22:00 +0300

    Adjust margin of Search component layout
    Increased bottom margin for the Search component.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,7 +3,7 @@ import FilterRegion from "../feature/FilterBar";
 
 export default function Search(){
   return(
-    <div className="flex flex-col sm:flex-row justify-between gap-4 h-12 mb-12  ">
+    <div className="flex flex-col sm:flex-row justify-between gap-4 h-12 mb-20 md:mb-12  ">
     <SearchBar />
     <FilterRegion />
     </div>
