eb6633bf9efcaf3930b3ebbb05a0af2bc568b5b3
Ruqia alsheikha <rokiash654@gmail.com>
2026-05-24 19:06:39 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import {useCountriesStore} from "../store/useCountriesStore";
 import {useEffect} from "react";
 import Cards from "../components/Cards";
 import Search from "../components/Search";
-import StatusHandler from "../components/StatusHandlers";
+import StatusHandlers from "../components/StatusHandlers";
 export default function Home(){
   const { LoadCountries } = useCountriesStore();
 
@@ -12,7 +12,7 @@ export default function Home(){
 
   return(
     <div className="min-h-screen bg-gray-50 text-gray-950 dark:bg-blue-950 dark:text-white transition-colors duration-300 px-10 md:px-20 py-10">
-      <StatusHandler />
+      <StatusHandlers />
       <Search />
       <Cards />
     </div>
