7fad22c1d63a40f858a503ae2ab867abbeaf3475
Ruqia alsheikha <rokiash654@gmail.com>
2026-09-08 06:52:29 +0300

    Update fetchCountries.js

--- src/api/fetchCountries.js
+++ src/api/fetchCountries.js
@@ -1,6 +1,6 @@
 export default async function fetchCountries(){
   try {
-  const response = await fetch('https://restcountries.com/v5/all?fields=name,capital,region,population,flags,cca3');
+  const response = await fetch('https://api.restcountries.com/countries/v5/all?fields=name,capital,region,population,flags,cca3');
   if (!response.ok) throw new Error('Failed to retrieve data from the server');
   const data = await response.json();
   return data;
